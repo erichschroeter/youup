f2cb229997fbf495996f7f91e8035b31f4a9c287
Erich Schroeter <erich.schroeter-ext@jci.com>
2023-03-27 14:23:11 -0500

    wip: ping support with surge-ping

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 
 use std::net::SocketAddr;
+use surge_ping::IcmpPacket;
 use tokio::net::TcpStream;
 use tokio::time::{sleep, Duration};
+
+async fn is_ping(addr: SocketAddr) -> bool {
+    // let payload = [0];
+    match surge_ping::ping("127.0.0.1".parse().unwrap(), &[0]).await {
+        Ok((IcmpPacket::V4(packet), duration)) => {
+            println!(
+                "{} bytes from {}: icmp_seq={} ttl={:?} time={:.2?}",
+                packet.get_size(),
+                packet.get_source(),
+                packet.get_sequence(),
+                packet.get_ttl(),
+                duration
+            );
+            return true;
+        }
+        Ok(_) => unreachable!(),
+        Err(e) => println!("{:?}", e),
+    };
+    false
+}
 
 async fn is_server_up(addr: SocketAddr) -> bool {
     let mut retries = 5; // number of retries
@@ -24,7 +45,8 @@ async fn is_server_up(addr: SocketAddr) -> bool {
 #[tokio::main]
 async fn main() {
     let addr = "127.0.0.1:8080".parse().unwrap(); // replace with your server address
-    let is_up = is_server_up(addr).await;
+    // let is_up = is_server_up(addr).await;
+    let is_up = is_ping(addr).await;
     if is_up {
         println!("Server {addr} is up");
     } else {
